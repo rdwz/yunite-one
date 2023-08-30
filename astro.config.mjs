10745b28883442c6102a4d2943f11735c6f607fb
Marc <8125196+rdwz@users.noreply.github.com>
2023-08-30 20:42:33 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { SITE } from './src/data/config';
+import { defineConfig } from 'astro/config';
+import sitemap from 'astro-sitemap';
+import compress from 'astro-compress';
+import compressor from 'astro-compressor';
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap(), compress(), compressor(), robotsTxt()],
+  site: SITE.url
+});
